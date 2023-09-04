8c4225834b7ffb630542d1ca81978ccf03bb9fc2
Darío Chinea Delgado <dario.chinea.external@atos.net>
2023-09-04 09:21:22 +0100

    Initail commit

---
+++ src/main/java/com/ufc/UfcBackApplication.java
@@ -0,0 +1,13 @@
+package com.ufc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfcBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfcBackApplication.class, args);
+	}
+
+}
